aab92872cdd7fa3522f1504d5f9eaba51ce79c36
Thomas Swann <tkswann@mac.com>
2016-09-22 01:20:26 -0500

    all components from react-101 ported over and abstracted to individual files

---
+++ views/CommentList.jsx
@@ -0,0 +1,20 @@
+const React = require('react')
+
+const CommentList = React.createClass({
+  render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return(
+        <Comment author={comment.author} key={comment.id}>
+           {comment.text}
+         </Comment>
+      );
+    });
+   return (
+     <div className="commentList">
+       {commentNodes}
+     </div>
+   );
+  }
+})
+
+module.exports = CommentList
