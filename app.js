4ed9c09ee47a2140413c87f80d53e372a9923630
Thomas Swann <tkswann@mac.com>
2016-09-22 01:01:00 -0500

    added comments to server config

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 'use strict'
-
+// third party modules
 const express = require('express')
+
+// app modules and vars
   ,      path = require('path')
   ,       app = express()
   ,      port = process.env.PORT || 3000
   ,    routes = require('./routes/index')
 
-
+// server config
 app.set('port', port)
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
@@ -14,6 +16,9 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use(routes)
 app.use(express.static(path.join(__dirname)))
 
+/**  TODO:  Error Handling **/
+
+// initiate sever on designated port
 app.listen(port, () => {
   console.log(`Now listeing one ${port}`)
 })
