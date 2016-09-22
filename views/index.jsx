5e855fd5abb6192f4195ef94a20a84984272212b
Thomas Swann <tkswann@mac.com>
2016-09-22 01:13:17 -0500

    comment box component rendering

--- views/index.jsx
+++ views/index.jsx
@@ -1,5 +1,6 @@
 const React = require('React')
 const DefaultLayout = require('./layouts/master')
+const CommentBox = require('./CommentBox')
 
 
 
@@ -7,6 +8,7 @@ const IndexComponent = React.createClass({
   render: function() {
     return (
       <DefaultLayout name={this.props.name}>
+        <CommentBox />
         <div>
           <h1>This was built {this.props.name}</h1>
         </div>
